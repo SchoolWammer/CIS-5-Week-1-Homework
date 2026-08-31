4323bd8c2944c9353f67accb64d99cdf9abb96d8
SchoolWammer <joedvb2006@gmail.com>
2026-08-30 19:28:14 -0700

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -4,14 +4,14 @@
 // Assignment 1 — Joshua Van Brunt
 
 int main() {
-	std::string name, last;
+	std::string name, last ;
   int years = 0;
-  std::cout << "First name: ";
+  std::cout << "First name:";
   std::cin >> name;
-  std::cout << "Last name";
+  std::cout << "Last name:";
   std::cin >> last;
   std::cout << "Years coding goal: ";
   std::cin >> years;
-  std::cout << name << last << ". Goal In " << years << "years\n";
+  std::cout << name << last << " . Goal in " << years << " years\n";
   return 0;
 }
